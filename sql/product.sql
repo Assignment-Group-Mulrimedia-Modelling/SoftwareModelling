f8299aa58a5785998896bb0016b1fe3f661eeed8
Mouzammil <jowaheermouz@gmail.com>
2020-04-14 23:50:17 +0400

    Update product.sql

--- sql/product.sql
+++ sql/product.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2020 at 09:41 PM
+-- Generation Time: Apr 14, 2020 at 09:49 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.27
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `aid1`
+-- Database: `pharmacy`
 --
 
 -- --------------------------------------------------------
@@ -33,18 +32,18 @@ CREATE TABLE `product` (
   `Name` varchar(50) NOT NULL,
   `Supplier` varchar(50) NOT NULL,
   `QtyBought` varchar(50) NOT NULL,
-  `Bouns` varchar(50) NOT NULL,
-  `Box Qty` varchar(50) NOT NULL,
-  `W.Price` varchar(50) NOT NULL,
-  `R.Price` varchar(50) NOT NULL,
-  `Exp Date` varchar(20) NOT NULL
+  `Bonus` varchar(50) NOT NULL,
+  `BoxQty` varchar(50) NOT NULL,
+  `WPrice` varchar(50) NOT NULL,
+  `RPrice` varchar(50) NOT NULL,
+  `Exp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`ItemNO`, `Name`, `Supplier`, `QtyBought`, `Bouns`, `Box Qty`, `W.Price`, `R.Price`, `Exp Date`) VALUES
+INSERT INTO `product` (`ItemNO`, `Name`, `Supplier`, `QtyBought`, `Bonus`, `BoxQty`, `WPrice`, `RPrice`, `Exp`) VALUES
 ('21538', 'Doliprane', 'DST-Ltd', '45', '0', '8', '45', '57', '03/21'),
 ('38694', 'Panadol', 'Unicorn-Trading', '3', '0', '8', '3', '5', '10/20');
 COMMIT;
