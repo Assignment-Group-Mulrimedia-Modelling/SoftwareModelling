2f0f0dddcad913830a51792da8407c080790e0ab
Mouzammil <jowaheermouz@gmail.com>
2020-04-16 18:10:09 +0400

    Update customer.sql

--- sql/customer.sql
+++ sql/customer.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2020 at 10:24 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Generation Time: Apr 16, 2020 at 04:09 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,25 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `customer` (
-  `ID` varchar(50) NOT NULL,
+  `ID` int(4) NOT NULL,
   `Surname` varchar(50) DEFAULT NULL,
   `Name` varchar(50) DEFAULT NULL,
   `Address` varchar(50) DEFAULT NULL,
-  `Phone Number` varchar(50) DEFAULT NULL,
+  `Number` varchar(50) DEFAULT NULL,
   `Sex` varchar(10) DEFAULT NULL,
-  `D.O.B` date DEFAULT NULL,
-  `Medical Issues` varchar(5000) DEFAULT NULL
+  `DOB` varchar(15) DEFAULT NULL,
+  `MedicalIssues` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`ID`, `Surname`, `Name`, `Address`, `Phone Number`, `Sex`, `D.O.B`, `Medical Issues`) VALUES
-('1', 'Edoo', 'Wasimah', 'Vacoas', '23454567', 'Female', '1999-05-12', 'Diabetes'),
-('2', 'Babooa', 'Yash', 'Triolet', '124511567', 'male', '1999-06-08', 'Hypertension'),
-('3', 'Edoo', 'Tausif', 'Vacoas', '1123454567', 'male', '1999-06-02', 'asdfghjk'),
-('4', 'Jowa', 'Muzam', 'RHILL', '29934545967', 'male', '1999-09-11', 'asdfghjk');
+INSERT INTO `customer` (`ID`, `Surname`, `Name`, `Address`, `Number`, `Sex`, `DOB`, `MedicalIssues`) VALUES
+(1, 'Edoo', 'Wasimah', 'Vacoas', '23454567', 'Female', '1999-05-12', 'Diabetes'),
+(2, 'Babooa', 'Yash', 'Triolet', '124511567', 'male', '1999-06-08', 'Hypertension'),
+(3, 'Edoo', 'Tausif', 'Vacoas', '1123454567', 'male', '1999-06-02', 'asdfghjk'),
+(4, 'Jowa', 'Mouzammil', 'Quatre Bornes', '56969696', 'Male', '22/06/99', 'Nothing'),
+(5, 'Bacsou', 'Zaynab', '31/03/98', '69696969', 'F', '31/03/98', 'Mental');
 
 --
 -- Indexes for dumped tables
@@ -58,6 +58,16 @@ INSERT INTO `customer` (`ID`, `Surname`, `Name`, `Address`, `Phone Number`, `Sex
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
