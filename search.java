7d997ea4cb4130780f1bfb85416096ad210ee881
Mouzammil <jowaheermouz@gmail.com>
2020-04-17 16:01:16 +0400

    Update search.java

--- search.java
+++ search.java
@@ -1,11 +1,10 @@
-//import java.awt.BorderLayout;
+import java.awt.BorderLayout;
 import java.sql.*;
-import java.text.SimpleDateFormat;
 
+//import net.proteanit.sql.DbUtils;
 import javax.swing.*;
 import java.awt.EventQueue;
 import java.util.ArrayList;
-import java.util.Date;
 
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
@@ -22,24 +21,22 @@ import javax.swing.border.LineBorder;
 import java.awt.SystemColor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
-import java.awt.Toolkit;
-import javax.swing.border.TitledBorder;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class search extends JFrame {
 
-
-
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-	private JLabel DateLbl;
-	private JLabel TimeLbl;
+	private JLabel lblShowTotal;
+	private JLabel lblShowNetTotal;
+	private JLabel lblDiscount;
+	private double total;
+	
+
 	/**
 	 * Launch the application.
 	 */
-	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -52,55 +49,56 @@ public class search extends JFrame {
 			}
 		});
 	}
-
 	Connection conn = null;
 	private JTextField textFieldSearch;
 	private JTable table_1;
 	private JTable table_2;
 	private JLabel lblSearch;
 	private JScrollPane scrollPane_1;
-	private JLabel lblInvoice;
 	private JButton btnAdd;
 	private JTextField textFieldQty;
-	private JPanel menu;
-	private JLabel lblMenu;
-	private JButton buttonStock;
-	private JButton buttonCustomer;
-	private JButton buttonProduct;
+	private JButton btnProduct;
+	private JTextField textFieldDiscount;
+	private JButton btnOk;
 	private JButton btnNewButton;
-	
-	
-
-	
-	
 	/**
 	 * Create the frame.
 	 */
 	public search() {
-
-		setTitle("Create Invoice");
-		setIconImage(Toolkit.getDefaultToolkit().getImage(search.class.getResource("/Images/Invoice.png")));
+		/*addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				showData();
+				
+				
+			}
+		})*/
+		;
 		conn = connection.dbConnector();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(380, 125, 900, 512);
+		setBounds(0,0,872,630);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setToolTipText("");
 		scrollPane.setBackground(new Color(255, 255, 255));
 		scrollPane.setForeground(new Color(255, 255, 255));
-		scrollPane.setBounds(275, 75, 575, 141);
+		scrollPane.setBounds(12, 23, 709, 191);
 		scrollPane.getViewport().setBackground(new Color(255,255,255));
 		
 		contentPane.add(scrollPane);
 		
 		
 		table = new JTable();
-		table.setBackground(Color.CYAN);
+		table.setBackground(new Color(245, 222, 179));
+		table.setSurrendersFocusOnKeystroke(true);
+		table.setShowVerticalLines(false);
+		table.setShowHorizontalLines(false);
 		scrollPane.setViewportView(table); 
 		
 		ArrayList<Item> product = new ArrayList<>();  
@@ -109,26 +107,24 @@ public class search extends JFrame {
 		textFieldSearch.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent arg0) {
-				
 				try {
-					
-					String query = "SELECT Name,Supplier,QtyBought,Bonus,BoxQty,WPrice,RPrice/BoxQty As UnitPrice FROM product";
+					String query = "SELECT * from product where Name = ? ";
 					PreparedStatement pst = conn.prepareStatement(query);
+					pst.setString(1, textFieldSearch.getText() );
+					
 					ResultSet rs = pst.executeQuery();
 					table.setModel(DbUtils.resultSetToTableModel(rs));
 					
 					
-					pst.close();
 					
-				} catch (Exception e) {
-					e.printStackTrace();
+				}catch (Exception Ex) {
+					Ex.printStackTrace();
 				}
+				
+				
 			}
 		});
-		
-
-
-		textFieldSearch.setBounds(360, 240, 130, 22);
+		textFieldSearch.setBounds(115, 236, 130, 22);
 		contentPane.add(textFieldSearch);
 		textFieldSearch.setColumns(10);
 		
@@ -136,177 +132,48 @@ public class search extends JFrame {
 		table_1.setBounds(148, 322, 1, 1);
 		contentPane.add(table_1);
 		
-		JPanel panel = new JPanel();
-		panel.setToolTipText("");
-		panel.setBounds(269, 267, 583, 164);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
 		scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(6, 16, 571, 158);
-		panel.add(scrollPane_1);
-		scrollPane_1.setForeground(Color.WHITE);
+		scrollPane_1.setForeground(new Color(255, 255, 255));
+		scrollPane_1.setBounds(12, 282, 709, 126);
 		scrollPane_1.getViewport().setBackground(new Color(255,255,255));
+		contentPane.add(scrollPane_1);
 		
 		table_2 = new JTable();
-		table_2.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		table_2.setBorder(new LineBorder(Color.RED, 2, true));
-		table_2.setShowGrid(false);
-		table_2.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-			}
-		));
 		scrollPane_1.setViewportView(table_2);
-		table_2.setBackground(Color.CYAN);
+		table_2.setBackground(new Color(255, 255, 255));
+		table_2.setShowGrid(false);
 		table_2.setShowHorizontalLines(false);
 		table_2.setShowVerticalLines(false);
 		
 		lblSearch = new JLabel("Name :");
 		lblSearch.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblSearch.setBounds(295, 222, 54, 56);
+		lblSearch.setBounds(49, 218, 54, 56);
 		contentPane.add(lblSearch);
 		
-		lblInvoice = new JLabel("Invoice");
-		lblInvoice.setHorizontalAlignment(SwingConstants.CENTER);
-		lblInvoice.setFont(new Font("Tahoma", Font.BOLD, 25));
-		lblInvoice.setBounds(450, 25, 236, 37);
-		contentPane.add(lblInvoice);
-		
-		
-		btnAdd = new JButton("add");
-		btnAdd.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int Quantity; //To store the value in the textbox
-				double UPrice;
-				try {
-					Quantity = Integer.parseInt(textFieldQty.getText());
-					String query = "SELECT * from product where Name = ? ";
-					PreparedStatement pst = conn.prepareStatement(query);
-					pst.setString(1, textFieldSearch.getText() );
-					
-					ResultSet rs = pst.executeQuery();
-					while(rs.next()) {
-						Item item = new Item();
-						
-						
-						item.setName(rs.getString("Name"));
-						item.setQty(Quantity);
-						item.setRPrice(rs.getDouble("RPrice"));
-						item.setBoxQty(rs.getInt("BoxQty"));
-						
-						product.add(item);
-				
-						
-					}
-					
-					table.setModel(DbUtils.resultSetToTableModel(rs));
-					
-					pst.close();
-				} catch (SQLException ex) {
-					// TODO Auto-generated catch block
-					ex.printStackTrace();
-				}
-				ArrayList <Object[]> inv = new ArrayList<Object[]>();
-				for (int i =0;i<product.size();i++) {
-					inv.add(new Object[] {
-							product.get(i).getQuantity(),
-							product.get(i).getName(),
-							product.get(i).getUPrice(),
-							product.get(i).getPrice()
-		
-					});
-				}
-				table_2.setModel(new DefaultTableModel(inv.toArray(new Object[][] {}),
-						new String[] {"Qty", "Name", "UPrice", "Item Total"}));
-			}
-
-		});
-		
-		btnAdd.setBounds(740, 239, 97, 25);
-		contentPane.add(btnAdd);
-		
 		textFieldQty = new JTextField();
-		textFieldQty.setBounds(570, 240, 116, 22);
+		textFieldQty.setBounds(396, 236, 116, 22);
 		contentPane.add(textFieldQty);
 		textFieldQty.setColumns(10);
 		
 		JLabel lblQty = new JLabel("Qty :");
 		lblQty.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblQty.setBounds(525, 242, 38, 16);
+		lblQty.setBounds(346, 237, 38, 16);
 		contentPane.add(lblQty);
 		
-		JPanel panelbackgroundInvoice = new JPanel();
-		panelbackgroundInvoice.setBorder(new LineBorder(SystemColor.window, 3));
-		panelbackgroundInvoice.setBackground(SystemColor.controlShadow);
-		panelbackgroundInvoice.setBounds(250, 15, 624, 429);
-		contentPane.add(panelbackgroundInvoice);
-		panelbackgroundInvoice.setLayout(null);
 		
-		menu = new JPanel();
-		menu.setBorder(new LineBorder(SystemColor.desktop, 2));
-		menu.setBackground(SystemColor.activeCaption);
-		menu.setBounds(25, 15, 192, 429);
-		contentPane.add(menu);
-		menu.setLayout(null);
+		btnAdd = new JButton("add");
+		btnAdd.setBounds(622, 234, 97, 25);
+		contentPane.add(btnAdd);
 		
-		lblMenu = new JLabel("Menu");
-		lblMenu.setForeground(new Color(0, 0, 255));
-		lblMenu.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMenu.setFont(new Font("Arial", Font.BOLD, 21));
-		lblMenu.setBounds(22, 22, 145, 45);
-		menu.add(lblMenu);
-		
-		buttonStock = new JButton("Stock");
-		buttonStock.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				stock stock1 = new stock();
-				stock1.setVisible(true);
-			}
-		});
-		buttonStock.setFont(new Font("Arial", Font.BOLD, 15));
-		buttonStock.setBounds(42, 98, 95, 30);
-		menu.add(buttonStock);
-		
-		buttonCustomer = new JButton("Customer");
-		buttonCustomer.setFont(new Font("Arial", Font.BOLD, 13));
-		buttonCustomer.setBounds(40, 198, 105, 30);
-		menu.add(buttonCustomer);
-		
-		buttonProduct = new JButton("Product");
-		buttonProduct.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Product product = new Product();
-				product.setVisible(true);
-			}
-		});
-		buttonProduct.setFont(new Font("Arial", Font.BOLD, 15));
-		buttonProduct.setBounds(42, 308, 95, 30);
-		menu.add(buttonProduct);
-		
-		btnNewButton = new JButton("Print");
-		btnNewButton.addActionListener(new ActionListener() {
+		btnProduct = new JButton("New Product");
+		btnProduct.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String printdata = panel.getUIClassID();
-				PrinterJob job = PrinterJob.getPrinterJob();
-				boolean doprint = job.printDialog();
-				if(doprint) {
-					try {
-						
-						job.print();
-						
-					}catch(PrinterException e2){
-						
-					}
-				}
+				ProductInv product = new ProductInv();
+				product.setVisible(true);
+				showData();
+				
 			}
 		});
-		btnNewButton.setBounds(761, 450, 89, 23);
-		contentPane.add(btnNewButton);
-		
 		JButton btnNewButton_1 = new JButton("Save");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -342,7 +209,193 @@ public class search extends JFrame {
 			}	
 			
 		});
-		btnNewButton_1.setBounds(656, 450, 89, 23);
+		btnNewButton_1.setBounds(115, 502, 89, 23);
 		contentPane.add(btnNewButton_1);
+		btnProduct.setBounds(733, 94, 110, 37);
+		contentPane.add(btnProduct);
+		
+		JLabel lblTotal = new JLabel("Total");
+		lblTotal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblTotal.setBounds(581, 437, 56, 16);
+		contentPane.add(lblTotal);
+		
+		lblDiscount = new JLabel("Discount");
+		lblDiscount.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblDiscount.setBounds(562, 464, 56, 16);
+		contentPane.add(lblDiscount);
+		
+		lblShowTotal = new JLabel("");
+		lblShowTotal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblShowTotal.setBounds(642, 437, 121, 16);
+		contentPane.add(lblShowTotal);
+		
+		textFieldDiscount = new JTextField();
+		textFieldDiscount = new JTextField("0.0");
+		textFieldDiscount.setFont(new Font("Tahoma", Font.BOLD, 13));
+		textFieldDiscount.setBounds(647, 463, 116, 22);
+		contentPane.add(textFieldDiscount);
+		textFieldDiscount.setColumns(10);
+		
+		JLabel lblNetTotal = new JLabel("Net Total");
+		lblNetTotal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNetTotal.setBounds(560, 505, 73, 16);
+		contentPane.add(lblNetTotal);
+		
+		lblShowNetTotal = new JLabel("");
+		lblShowNetTotal.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblShowNetTotal.setBounds(642, 505, 121, 16);
+		contentPane.add(lblShowNetTotal);
+		
+		btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showNetTotal(total);
+			}
+		});
+		btnOk.setBounds(49, 433, 97, 25);
+		contentPane.add(btnOk);
+		
+		btnNewButton = new JButton("Cancel");
+		btnNewButton.setBounds(201, 433, 97, 25);
+		contentPane.add(btnNewButton);
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				int Quantity; //To store the value in the textbox
+				double UPrice;
+				try {
+					Quantity = Integer.parseInt(textFieldQty.getText());
+					String query = "SELECT * from product where Name = ? ";
+					PreparedStatement pst = conn.prepareStatement(query);
+					pst.setString(1, textFieldSearch.getText() );
+					
+					ResultSet rs = pst.executeQuery();
+					while(rs.next()) {
+						Item item = new Item();
+						
+						item.setQty(Quantity);
+						item.setName(rs.getString("Name"));
+						item.setRPrice(rs.getDouble("RPrice"));
+						item.setBoxQty(rs.getInt("BoxQty"));
+						
+						product.add(item);
+				
+						
+					}
+					
+					
+					
+					pst.close();
+				} catch (SQLException ex) {
+					// TODO Auto-generated catch block
+					ex.printStackTrace();
+				}
+				showData();
+				textFieldSearch.setText("");
+				textFieldQty.setText("");
+				ArrayList <Object[]> inv = new ArrayList<Object[]>();
+				total =0;
+				for (int i =0;i<product.size();i++) {
+					inv.add(new Object[] {
+							product.get(i).getQuantity(),
+							product.get(i).getName(),
+							product.get(i).getUPrice(),
+							product.get(i).getPrice()
+					});
+					total += product.get(i).getPrice();
+					showTotal (total);
+					//showNetTotal(total);
+				}
+				
+				
+				
+				table_2.setModel(new DefaultTableModel(inv.toArray(new Object[][] {}),
+						new String[] {"Qty", "Name", "UPrice", "Item Total"}));
+				
+				
+			}
+			
+			
+		});
+		
+	}
+	
+	private void showTotal(double total) { //method to show total
+		//Integer.parseInt
+		String totalStr = Double.toString(total); //converting a double to string 
+		lblShowTotal.setText(totalStr); 
+		
+	}
+	
+	private void showNetTotal(double total) {
+		double discount;
+		double netTotal;
+		String netTotalStr;
+		discount = Double.parseDouble(textFieldDiscount.getText());
+		netTotal = total - discount;
+		netTotalStr = Double.toString(netTotal);
+		lblShowNetTotal.setText(netTotalStr); 
+		
+		
+	}
+	private void showData() { //method to show current contents of database 
+		Connection conn = connection.dbConnector();
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("ID");
+		model.addColumn("Name");
+		model.addColumn("Supplier");
+		model.addColumn("Qty");
+		model.addColumn("Bonus");
+		model.addColumn("BoxQty");
+		model.addColumn("W.Price");
+		model.addColumn("R.Price");
+		model.addColumn("Expiry");
+		model.addColumn("Stock");
+		
+		try {
+			
+			String query = "SELECT * FROM product";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+			while (rs.next()) { //getting each row from database
+				
+				model.addRow(new Object[] {
+						rs.getString("ItemNO"),
+						rs.getString("Name"),
+						rs.getString("Supplier"),
+						rs.getString("QtyBought"),
+						rs.getString("Bonus"),
+						rs.getString("BoxQty"),
+						rs.getString("WPrice"),
+						rs.getString("RPrice"),
+						rs.getString("Exp"),
+						rs.getString("Stock"),
+						
+						
+				});
+			}
+			rs.close();
+			st.close();
+			conn.close();
+			
+			table.setModel(model);
+			table.setAutoResizeMode(0);
+			table.getColumnModel().getColumn(0).setPreferredWidth(50); //setting width of each individual columns
+			table.getColumnModel().getColumn(1).setPreferredWidth(120);
+			table.getColumnModel().getColumn(2).setPreferredWidth(120);
+			table.getColumnModel().getColumn(3).setPreferredWidth(50);
+			table.getColumnModel().getColumn(4).setPreferredWidth(50);
+			table.getColumnModel().getColumn(5).setPreferredWidth(60);
+			table.getColumnModel().getColumn(6).setPreferredWidth(60);
+			table.getColumnModel().getColumn(7).setPreferredWidth(60);
+			table.getColumnModel().getColumn(8).setPreferredWidth(60);
+			table.getColumnModel().getColumn(9).setPreferredWidth(60);
+			
+			
+			
+			
+		} catch(Exception e1) {
+			System.out.println("Error: " + e1);
+		}
 	}
 }
